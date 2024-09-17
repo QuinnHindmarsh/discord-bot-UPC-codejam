1309353ddc86b39344a61acb2fbc89bc0ffd67fb
QuinnHindmarsh <qhindmarsh@gmail.com>
2024-09-17 20:09:02 +0930

    added functionality to message everyone not signed up

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #   FEATURES
 #   !member @user - lets you add member role to mentioned person
 
-from discord import Intents, Client, Message, utils
+from discord import Intents, Client, Message, utils, Member
 from private import token
 from responses import get_response
 from copy import deepcopy
@@ -14,9 +14,8 @@ intents.members = True  # NQQA
 
 client: Client = Client(intents=intents)
 
+
 # check for specific role
-
-
 async def check_for_role(author, roleName):
     author_roles = author.roles
     flag = False
@@ -25,9 +24,8 @@ async def check_for_role(author, roleName):
             flag = True
     return flag
 
+
 # strips all text from userID+
-
-
 def strip_UID(text):
     textToRemove = ['!member ', '<@', '>']
     UID = text
@@ -39,6 +37,8 @@ def strip_UID(text):
 
 
 # adds role to mentioned member
+
+
 async def add_role(message: Message, roleName='member'):
 
     if await check_for_role(message.author, 'exec') == True:
@@ -57,7 +57,7 @@ async def add_role(message: Message, roleName='member'):
 
 # messages every member - used to remined people of renewal
 async def renew():
-    if datetime.now().month != 9:
+    if datetime.now().month != 1:
         return 'It is not January, you dont wanna message everyone yet'
     allUsers = client.get_all_members()
     for user in allUsers:
@@ -67,9 +67,26 @@ async def renew():
             await channel.send('Hi,\nThis message has been automatically sent to all members to remined you that your membership to the UniSA Programming community automatically expires on the 1st of January. To continue to partipate in our events please renew it.\nThanks!\nhttps://usasa.sa.edu.au/clubs/join/7520/')
     return 'All members have been reminded of there expiring membership '
 
+
+# messages people who have been in the server for more then a week and dont have member or other relivent role to sign up
+async def msg_non_members():
+    allUsers = client.get_all_members()
+    nonMembers = []
+
+    for user in allUsers:
+        if await check_for_role(user, 'member') == False and await check_for_role(user, 'Adelaide CSC exec') == False and await check_for_role(user, 'Industry') == False:
+            if (datetime.now() - (user.joined_at.replace(tzinfo=None))).days >= 7:
+                nonMembers.append(user)
+
+    for user in nonMembers:
+        if isinstance(user, Member):
+            channel = await user.create_dm()
+            await channel.send('Hi,\nyou have been in the UniSA programming community discord for more then a week, and still have not signed up.\nIt only takes a minute to sign up, is free and you dont need to be a UniSA student to do it. Signing up is the best way to support our club and allow us to host as many future events as possible.\nhttps://usasa.sa.edu.au/clubs/join/7520/')
+
+    return f'{[x.name for x in nonMembers]}'
+
+
 # finds the appropraite function to call based on the message content
-
-
 async def get_response(message: Message):
     message_content = message.content
     if message_content[0] != '!':
@@ -77,8 +94,10 @@ async def get_response(message: Message):
 
     if message_content.startswith('!member'):
         return await add_role(message)
-    if message_content.startswith('!spam'):
+    elif message_content.startswith('!spam'):
         return await renew()
+    elif message_content.startswith('!msgnonmembers'):
+        return await msg_non_members()
 
 
 # deals with recived messages
@@ -107,6 +126,11 @@ async def on_message(message: Message):
         return
 
     await send_message(message)
+
+
+@client.event
+async def on_raw_member_remove(payload):
+    print(payload)
 
 
 #  Runs bot
